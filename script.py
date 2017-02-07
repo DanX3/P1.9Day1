01775db95dbf614dc531e26fe348610604226f45
Daniele Tolomelli <daniel.tolomelli@gmail.com>
2017-02-07 10:21:55 +0100

    merged with polynomial

--- script.py
+++ script.py
@@ -9,6 +9,8 @@ print "3 - y = sqrt(x)"
 print "4 - y = sin(x)"
 print "5 - y = cos(x)"
 print "6 - y = tan(x)"
+print "7 - y = x^2"
+print "8 - y = x^3"
 choice = input();
 
 if choice == 1:
@@ -23,6 +25,10 @@ if choice == 5:
     f = lambda x: np.cos(x)
 if choice == 6:
     f = lambda x: np.tan(x)
+if choice == 7:
+    f = lambda x: x**2
+if choice == 8:
+    f = lambda x: x**3
 
 yval = f(xval)
 
