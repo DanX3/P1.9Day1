5600a1793b2da08a54a255abd0a5689659b69b21
Daniele Tolomelli <daniel.tolomelli@gmail.com>
2017-02-07 09:45:14 +0100

    added irrational functions

--- script.py
+++ script.py
@@ -4,10 +4,16 @@ import matplotlib.pyplot as pl
 xval = np.linspace(-5, 5, 100)
 print "Usage:"
 print "1 - y = x"
+print "2 - y = exp(x)"
+print "3 - y = sqrt(x)"
 choice = input();
 
 if choice == 1:
     f = lambda x: x
+if choice == 2:
+    f = lambda x: np.exp(x)
+if choice == 3:
+    f = lambda x: np.sqrt(x)
 
 yval = f(xval)
 
