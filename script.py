10b79305ff17b5f0a9e089eceab45901cb00b419
Daniele Tolomelli <daniel.tolomelli@gmail.com>
2017-02-07 09:39:30 +0100

    added trogonometric functions

--- script.py
+++ script.py
@@ -7,7 +7,11 @@ choice = input();
 if choice == 1:
     f = lambda x: x
 if choice == 2:
-    f = lambda x: x**2 + 1
+    f = lambda x: np.sin(x)
+if choice == 3:
+    f = lambda x: np.cos(x)
+if choice == 4:
+    f = lambda x: np.tan(x)
 
 yval = f(xval)
 
