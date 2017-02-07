40937e71004614810796af19596fb820bfd7f587
Daniele Tolomelli <daniel.tolomelli@gmail.com>
2017-02-07 09:43:24 +0100

    added usage instructions

--- script.py
+++ script.py
@@ -2,12 +2,12 @@ import numpy as np
 import matplotlib.pyplot as pl
 
 xval = np.linspace(-5, 5, 100)
+print "Usage:"
+print "1 - y = x"
 choice = input();
 
 if choice == 1:
     f = lambda x: x
-if choice == 2:
-    f = lambda x: x**2 + 1
 
 yval = f(xval)
 
